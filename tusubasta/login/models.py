bfa73c05a87c46445c07275382ce58354a31eaf1
AlexisImhoff <alexisr.imhoff@gmail.com>
2017-04-25 19:06:15 -0300

    Se modifica models.py

--- tusubasta/login/models.py
+++ tusubasta/login/models.py
@@ -1,19 +1,50 @@
 # -*- encoding: utf-8 -*-
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from sitio.models import *
 
 # class estadosPerfil(models.Model):
 # descripcion = models.CharField(max_length=50, null=True,blank=True)
-
-
-class perfil(models.Model):
+'''
+class Usuarios(AbstractBaseUser):
+	
+	nick = models.CharField(max_length=50, null=True, blank=True)
 	direccion = models.CharField(max_length=50, null=True, blank=True)
 	direccionNumero = models.IntegerField(default=0)
-	# ciudad = models.ForeignKey(null=True,blank= True)
-	fechaNacimiento = models.DateField(null=True, blank=True)
-	# estado = models.ForeignKey(estadosPerfil)
-	usuario = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+	#ciudad = models.ForeignKey(null=True,blank= True)
+	fechaNacimiento = models.DateField(null=True, blank=True)	
+
+		objects = MyUserManager()
 
 	def __str__(self):
-		return self.nick
+			return self.nick
+
+'''
+class EstadosUsuarios(models.Model):
+	descripcion			= models.CharField(max_length=50, null=True, blank=True)
+
+	def __str__(self):
+		return self.descripcion
+
+
+class TiposUsuarios(models.Model):
+	descripcion			=  models.CharField(max_length=50, null=True, blank=True)
+
+	def __str__(self):
+		return self.descripcion
+
+
+class Perfil(models.Model):
+	direccion 			= models.CharField(max_length=50, null=True, blank=True)
+	direccionNumero 	= models.IntegerField(default=0)
+	ciudad				= models.CharField(max_length=50, null=True, blank=True)
+	provincia			= models.CharField(max_length=50, null=True, blank=True)
+	pais				= models.CharField(max_length=50, null=True, blank=True)
+	fechaNacimiento 	= models.DateField(null=True, blank=True)
+	# estado = models.ForeignKey(estadosPerfil)
+	usuario 			= models.OneToOneField(User,on_delete = models.CASCADE, null=True, blank=True)
+	idEstadoUsuario		= models.ForeignKey(EstadosUsuarios, null=True, blank=True)
+	idTipoUsuario		= models.ForeignKey(TiposUsuarios, null=True, blank=True)
+
+	def __str__(self):
+		return self.usuario
--- tusubasta/sitio/models.py
+++ tusubasta/sitio/models.py
@@ -5,31 +5,97 @@ import datetime
 
 # Create your models here.
 
-class categoria(models.Model):
-	descripcion = models.CharField(max_length=50,null=True,blank=True)
+class Categorias(models.Model):
+	descripcion	 		= models.CharField(max_length=50,null=True,blank=True)
 
-class subCategoria(models.Model):
-	descripcion = models.CharField(max_length=50,null=True,blank=True)
-	idCategoria = models.ForeignKey(categoria)
-
-class calificacion(models.Model):
-	descripcion = models.CharField(max_length=50,null=True,blank=True)
-
-class cabSubasta(models.Model):
-	descripcion = models.TextField(null=True,blank=True)
-	precioBase = models.DecimalField(max_digits=7, decimal_places=2,null=True,blank=True)
-	fechaAlta = models.DateTimeField(auto_now=True, null=True,blank=True)
-	fechaBaja = models.DateTimeField(auto_now=True, null=True,blank=True)
-	usuarioVendedor = models.OneToOneField(User)
-	imagen = models.ImageField(null=True,blank=True) #upload_to='photo'
-	idSubcategoria = models.ForeignKey(subCategoria)
-	idCalificacion = models.ForeignKey(calificacion)
-
-class detSubasta(models.Model):
-	idSubasta = models.ForeignKey(cabSubasta)
-	usuarioComprador = models.ForeignKey(User)
-	valorOferta = models.DecimalField(max_digits=7, decimal_places=2,null=True,blank=True)
-	fechaOferta = models.DateTimeField(auto_now=True, null=True,blank=True)
-	estado = models.BooleanField(default=True, null=False)
+	def __str__(self):
+		return self.descripcion
 
 
+class SubCategorias(models.Model):
+	descripcion 		= models.CharField(max_length=50,null=True,blank=True)
+	idCategoria 		= models.ForeignKey(Categorias,null=True,blank=True)
+
+	def __str__(self):
+		return self.descripcion
+
+
+class Calificaciones(models.Model):
+	descripcion 		= models.CharField(max_length=50,null=True,blank=True)
+	fechaAlta			= models.DateTimeField(auto_now=True, null=True,blank=True)
+	idUsuarioComprador	= models.ForeignKey(User,null=True,blank=True)
+
+	def __str__(self):
+		return self.descripcion
+
+
+class Subastas(models.Model):
+	detalle 			= models.TextField(null=True,blank=True)
+	precioBase 			= models.DecimalField(max_digits=7, decimal_places=2,null=True,blank=True)
+	fechaAlta 			= models.DateTimeField(auto_now=True, null=True,blank=True)
+	fechaBaja 			= models.DateTimeField(auto_now=True, null=True,blank=True)
+	fechaFin 			= models.DateTimeField(auto_now=True, null=True,blank=True)
+	usuarioVendedor 	= models.ForeignKey(Categorias,null=True,blank=True)
+	idSubcategoria 		= models.ForeignKey(SubCategorias,null=True,blank=True)
+	idCalificacion 		= models.ForeignKey(Calificaciones,null=True,blank=True)
+	#######VERRRR#######
+	idUsuarioVendedor	= models.ForeignKey(User,null=True,blank=True)
+	localidad			= models.CharField(max_length=50,null=True,blank=True)
+	provincia			= models.CharField(max_length=50,null=True,blank=True)
+	pais				= models.CharField(max_length=50,null=True,blank=True)
+
+
+	def __str__(self):
+		return self.detalle
+
+
+class Ofertas(models.Model):
+	idSubasta 			= models.ForeignKey(Subastas, null=True, blank=True)
+	usuarioComprador 	= models.ForeignKey(User, null=True, blank=True)
+	valorOferta 		= models.DecimalField(max_digits=7, decimal_places=2,null=True,blank=True)
+	fechaOferta			= models.DateTimeField(auto_now=True, null=True,blank=True)
+	ganador 			= models.BooleanField(default=True, null=False)
+
+	def __str__(self):
+		return self.valorOferta
+
+
+class Comentarios(models.Model):
+	idSubasta 			= models.ForeignKey(Subastas, null=True, blank=True)
+	comentario 			= models.TextField(null=True,blank=True)
+	idUsuario			= models.ForeignKey(User,null=True,blank=True)	
+	fechaBaja 			= models.DateTimeField(auto_now=True, null=True,blank=True)
+	fechaAlta 			= models.DateTimeField(auto_now=True, null=True,blank=True)
+
+	def __str__(self):
+			return self.comentario
+
+
+class Imagenes(models.Model):
+	idSubasta 			= models.ForeignKey(Subastas)
+	descripcion 		= models.CharField(max_length=50,null=True,blank=True)
+	URL 				= models.CharField(max_length=150,null=True,blank=True)
+
+	def __str__(self):
+		return self.descripcion
+		
+
+class Respuestas(models.Model):
+	idComentario		= models.ForeignKey(Comentarios, null=True, blank=True)
+	idUsuario			= models.ForeignKey(User,null=True,blank=True)	
+	respuesta			= models.TextField(null=True,blank=True)
+	fechaBaja 			= models.DateTimeField(auto_now=True, null=True,blank=True)
+	fechaAlta 			= models.DateTimeField(auto_now=True, null=True,blank=True)
+
+	def __str__(self):
+		return self.respuesta
+
+
+class Likes(models.Model):
+	idSubasta 			= models.ForeignKey(Subastas, null=True, blank=True)
+	idUsuarioVendedor	= models.ForeignKey(User,null=True,blank=True)	
+	fechaBaja 			= models.DateTimeField(auto_now=True, null=True,blank=True)
+	fechaAlta 			= models.DateTimeField(auto_now=True, null=True,blank=True)
+
+	def __str__(self):
+		return self.fechaAlta
